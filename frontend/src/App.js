5dcac9cdef4a0d19d98c867e7ebd427425d2d09c
kyawmt <kmt@kmtkko.com>
2021-07-03 22:08:38 +0800

    Routing for react js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter, Switch, Route }  from "react-router-dom";
 import FibComponent from './components/FibComponent';
 
 function App() {
   return (
-    <div className="App">
-      <FibComponent/>
-    </div>
+    
+  <BrowserRouter>
+  <Switch>
+
+    <Route path="/fibonacci" component={FibComponent}/>
+    
+  </Switch>
+  </BrowserRouter>
   );
 }
 
